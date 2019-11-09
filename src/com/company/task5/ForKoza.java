daffd30f556d69487e6a308de5dc1408b5197f87
Alexey Korotkih <lexa.36484@gmail.com>
2019-11-09 19:16:45 +0500

    head first java

--- src/com/company/task5/ForKoza.java
+++ src/com/company/task5/ForKoza.java
@@ -13,7 +13,7 @@ public class ForKoza implements ActionListener {
 
     public void actionPerformed(ActionEvent event)  {
 
-        text.setText("Таня коза");
+        text.setText("Тасяна");
 
 
     }
